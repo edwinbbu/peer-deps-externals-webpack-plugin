b18348e8d6ef524e3fe7c9a4338ea86152f1d30f
Edwin Babu <edwinbbu@gmail.com>
2021-11-25 11:58:08 +0530

    Webpack 5 support added

--- src/index.js
+++ src/index.js
@@ -1,39 +1,41 @@
-'use strict';
+"use strict";
 
-const ExternalModuleFactoryPlugin = require('webpack/lib/ExternalModuleFactoryPlugin');
+const ExternalModuleFactoryPlugin = require("webpack/lib/ExternalModuleFactoryPlugin");
 
 class PeerDepsExternalsPlugin {
   apply(compiler) {
     const peerDependencies = getPeerDependencies();
 
+    // webpack 5+
+    if (typeof compiler.options.output.library === "object") {
+      compiler.hooks.compile.tap(
+        "PeerDepsExternalsPlugin",
+        ({ normalModuleFactory }) => {
+          new ExternalModuleFactoryPlugin(
+            compiler.options.output.library.type,
+            peerDependencies
+          ).apply(normalModuleFactory);
+        }
+      );
+    }
     // webpack 4+
-    if (compiler.hooks) {
-      compiler.hooks.compile.tap('compile', params => {
+    else {
+      compiler.hooks.compile.tap("compile", (params) => {
         new ExternalModuleFactoryPlugin(
           compiler.options.output.libraryTarget,
           peerDependencies
         ).apply(params.normalModuleFactory);
       });
-    // webpack < 4, remove this in next major version
-    } else {
-      compiler.plugin('compile', params => {
-        params.normalModuleFactory.apply(
-          new ExternalModuleFactoryPlugin(
-            compiler.options.output.libraryTarget,
-            peerDependencies
-          )
-        );
-      });  
     }
   }
 }
 
 function getPeerDependencies() {
   try {
-    const { resolve } = require('path');
-    const pkg = require(resolve(process.cwd(), 'package.json'));
+    const { resolve } = require("path");
+    const pkg = require(resolve(process.cwd(), "package.json"));
     return Object.keys(pkg.peerDependencies);
-  } catch(err) {
+  } catch (err) {
     return [];
   }
 }
